46f7ce5232d5e28d4d6889d82ef38dfe7032cdd9
nachos5 <gnorland93@gmail.com>
2020-06-10 03:51:56 +0000

    x

--- src/coverage.ts
+++ src/coverage.ts
@@ -1,10 +1,26 @@
 import * as puppeteer from 'puppeteer';
 
-async function getCoverage() {
-  console.info(puppeteer);
-  const browser = await puppeteer.launch();
+async function getCoverage(): Promise<void> {
+  const browser = await puppeteer.launch({
+    executablePath:
+      'node_modules/puppeteer/.local-chromium/linux-756035/chrome-linux/chrome',
+  });
+
+  console.info('setup');
   const page = await browser.newPage();
   const client = await page.target().createCDPSession();
+  await client.send('Page.enable');
+  await client.send('DOM.enable');
+  await client.send('CSS.enable');
+
+  console.info('coverage tracking');
+  await client.send('CSS.startRuleUsageTracking');
+  await page.goto('https://www.sminor.is');
+  const rules = await client.send('CSS.takeCoverageDelta');
+  const usedRules = rules.coverage.filter((rule) => {
+    return rule.used;
+  });
+  console.info(usedRules);
 }
 
 export default getCoverage;
